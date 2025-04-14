7930d8238f32f29e269ee7c9a3ac0a5aaf392ef8
Albert-Andrews <aaaquaye001@st.ug.edu.gh>
2025-04-14 12:45:32 +0000

    feat: implement page layout with TypingAnimation and InteractiveHoverButton components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { TypingAnimation } from "../components/magicui/typing-animation";
+import { InteractiveHoverButton } from "../components/magicui/interactive-hover-button";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="text-3xl flex flex-col justify-center items-center h-screen gap-[2rem]">
+      <TypingAnimation className="text-6xl">Just Do It</TypingAnimation>
+
+      <InteractiveHoverButton>Start</InteractiveHoverButton>
+    </div>
+  );
 }
