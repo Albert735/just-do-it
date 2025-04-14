cc00a3a2836e98b8b10256af138b838116696d28
Albert-Andrews <aaaquaye001@st.ug.edu.gh>
2025-04-14 12:52:01 +0000

    feat: add DotPattern component and integrate into page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { TypingAnimation } from "../components/magicui/typing-animation";
 import { InteractiveHoverButton } from "../components/magicui/interactive-hover-button";
+import { DotPattern } from "../components/magicui/dot-pattern";
 
 export default function page() {
   return (
     <div className="text-3xl flex flex-col justify-center items-center h-screen gap-[2rem]">
-      <TypingAnimation className="text-6xl">Just Do It</TypingAnimation>
+      <DotPattern
+        glow={true}
+        className="[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]"
+      />
+      <TypingAnimation className="text-[5rem]">Just Do It</TypingAnimation>
 
       <InteractiveHoverButton>Start</InteractiveHoverButton>
     </div>
